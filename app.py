f5e43beb3ca823de5e155501b09dcb9a65a7178f
Maxim Kirenenko <part1zancheg@gmail.com>
2015-03-19 09:33:38 +0000

    Something went wrong

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, Markup, escape
 import db
 import re
 
@@ -14,11 +14,8 @@ def main():
 @app.route('/<int:pasteid>')
 def pasteid(pasteid):
 	paste = db.get_paste(pasteid)
-	paste = re.sub('	', '    ', paste)
-	paste = re.sub(' ', '&nbsp;', paste)
-	paste = re.sub('<', '&lt;', paste)
-	paste = re.sub('>', '&gt;', paste)
-	paste = re.sub('[\n\r]', '<br />', paste)
+	paste = str(escape(paste)).replace('	', ' ').replace(' ', '&nbsp;').replace('\n', '<br />')
+	
 	return paste
 
 
