260cb9589f8795cf8a05518fdfa120db0aad9e30
Maxim Kirenenko <part1zancheg@gmail.com>
2016-02-19 11:37:41 +0000

    a bunch of stupid bugs fixed

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ actions = ['plus', 'minus', 'multiplied by']
 
 def perform_math(arg1, arg2, action):
 	if action == 'plus':
-		return int(arg1) + int(arg2)
+		return numbers.index(arg1)+ numbers.index(arg2)
 	elif action == 'minus':
-		return int(arg1) - int(arg2)
+		return numbers.index(arg1) - numbers.index(arg2)
 	else:
-		return int(arg1) * int(arg2)
+		return numbers.index(arg1) * numbers.index(arg2)
 
 
 app = Flask(__name__)
@@ -40,6 +40,9 @@ def pasteid(pasteid):
 @app.route('/new', methods=['POST', 'GET']) # FIXME :: template for form, bootstrap form
 def form():
 	if request.method == 'POST':
+		arg1 = request.form['arg1']
+		arg2 = request.form['arg2']
+		action = request.form['action']
 		if perform_math(arg1, arg2, action) == int(request.form['result']):
 			return redirect('{0}{1}'.format(request.url_root, db.new_paste(request.form['paste'])))
 		return 'wrong captcha'
@@ -51,7 +54,7 @@ def form():
 	<input type="hidden" name="arg2" value="{arg2}" />
 	<input type="hidden" name="action" value="{action}" />
 	<input type="submit" value="Paste" />
-</form>'''.format(arg1=numbers[random.randint(0, len(numbers)-1)], arg2=numbers[random.randint(0, len(numbers)-1)])
+</form>'''.format(arg1=numbers[random.randint(0, len(numbers)-1)], arg2=numbers[random.randint(0, len(numbers)-1)], action=actions[random.randint(0, len(actions)-1)])
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0', port=8080)
